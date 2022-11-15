6ba79e21dc582b2d8a399895ccdd962878363c17
LaxrLee <stangathagu95@gmail.com>
2022-11-15 19:30:26 +0300

    upload string

--- leads/models.py
+++ leads/models.py
@@ -29,6 +29,8 @@ class Lead(models.Model):
     # profile_picture = models.ImageField(blank=True, null=True)
     # special_files = models.FileField(blank=True, null=True) 
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
    
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
